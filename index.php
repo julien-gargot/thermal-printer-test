25df7597b23e6c01cd98b2e57a335cb4385eaa6e
Julien Gargot <julien@g-u-i.me>
2016-12-12 14:14:54 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Thermal Printer</title>
+    <!-- <link rel="stylesheet" href="styles.css"> -->
+    <link rel="stylesheet" href="structure.css">
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+
+    <div class="page">
+
+      <p>
+        <strong>Bitmap gradient</strong><br>
+        <small>geometric</small>
+      </p>
+
+      <!-- <p>abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ</p> -->
+
+      <!-- <p>
+        Àà, Ââ, Ææ<br>
+        Çç<br>
+        Éé, Èè, Êê, Ëë<br>
+        Îî, Ïï<br>
+        Ôô, Œœ<br>
+        Ùù, Ûû, Üü<br>
+        Ÿÿ
+      </p> -->
+
+      <!-- <img src="placeholder-portrait.png" alt=""> -->
+
+      <!-- <?php $i=0; while ($i < 10) :?>
+      <p style="font-size: <?= $i+4 ?>pt;">
+        <?= $i+4 ?>pt — abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+      </p>
+      <?php $i++; endwhile; ?> -->
+
+      <!-- <?php $i=0; while ($i < 100) : $i++; ?>
+      <div class="line"
+           style="background-color: rgba(0,0,0,<?= $i/100 ?>);<?php if($i>50): ?>color:white;<?php endif; ?>">
+        <?= $i ?>%
+      </div>
+      <?php endwhile; ?> -->
+
+      <img src="trame-geometrique.gif" alt="">
+
+      <!-- <div id="gradient"></div> -->
+
+    </div>
+
+  </body>
+</html>
